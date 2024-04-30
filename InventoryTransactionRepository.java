0e8a44e194acc397ed63311cfc51b82e29278ad3
aayush-king <104244364+aayush-king@users.noreply.github.com>
2024-04-30 19:53:12 +0530

    Create InventoryTransactionRepository.java

---
+++ InventoryTransactionRepository.java
@@ -0,0 +1,7 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryTransactionRepository extends JpaRepository<InventoryTransaction, Long> {
+    List<InventoryTransaction> findByProductId(Long productId);
+}
